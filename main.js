0e3e7ece0d5567776b19a25b0caf41dffc1f8b03
carlo fenaroli <carlo.fenaroli@gmail.com>
2023-12-14 21:35:19 +0100

    creato pulsante mostra e nascondi contatti, e pulsante CreaContatto con alert

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 let contenitoreContatti = document.querySelector("#contenitoreContatti");
-let mostraContattiBtn = document.querySelector("#mostraContattiBtn");
+let mostraContattiBtn = document.querySelector("#btn_Mostra"); // Modificato il selettore per adattarlo all'ID del pulsante
 let inputNome = document.querySelector("#inputNome");
-let inputNumero = document.querySelector("#inputNumero")
+let inputNumero = document.querySelector("#inputNumero");
 const rubrica = {
-    'contatti' : [
-        {nome : "Leonardo", numero : "34694030394"},
-        {nome : "Giulio", numero : "344594033"},
-        {nome : "Alessio", numero : "346958749003"},
-        {nome : "Caterina", numero : "33455940303"},
+    'contatti': [
+        { nome: "Leonardo", numero: "34694030394" },
+        { nome: "Giulio", numero: "344594033" },
+        { nome: "Alessio", numero: "346958749003" },
+        { nome: "Caterina", numero: "334584737933" },
+        { nome: "Carlo", numero: "348474934" },
+        { nome: "Flogert", numero: "348374739" },
+        { nome: "Antonio", numero: "348483092302" },
+        { nome: "Sayo", numero: "3353625253256" },
     ],
-    mostraContatti : function(){
+    mostraContatti: function () {
         this.contatti.forEach((contatto) => {
             let cardContatto = document.createElement("div");
             cardContatto.classList.add("background-secondary", "p-5", "col-custom");
@@ -23,16 +27,34 @@ const rubrica = {
 }
 rubrica.mostraContatti();
 let check = false;
-mostraContattiBtn.addEventListener("click", ()=> {
-    if(!check){
+mostraContattiBtn.addEventListener("click", () => {
+    if (!check) {
         contenitoreContatti.innerHTML = "";
         rubrica.mostraContatti();
         check = true;
         mostraContattiBtn.innerHTML = "Nascondi Contatti";
-    }else{
+    } else {
         contenitoreContatti.innerHTML = "";
         check = false;
         mostraContattiBtn.innerHTML = "Mostra Contatti";
     }
-    
 })
+document.querySelector("#btn_CreaContatto").addEventListener("click", () => {
+    const nuovoNome = inputNome.value;
+    const nuovoNumero = inputNumero.value;
+
+    if (nuovoNome && nuovoNumero) {
+        const nuovoContatto = { nome: nuovoNome, numero: nuovoNumero };
+
+        rubrica.contatti.push(nuovoContatto);
+
+        contenitoreContatti.innerHTML = "";
+        rubrica.mostraContatti();
+
+        inputNome.value = "";
+        inputNumero.value = "";
+    } else {
+        alert("Inserisci un nome e un numero per il nuovo contatto.");
+    }
+});
+
