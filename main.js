074e0d40af51cacffc1289f9f3ee7d1e4341f45f
carlo fenaroli <carlo.fenaroli@gmail.com>
2023-12-15 20:11:56 +0100

    INseriti commenti al codice e revisionato pulsante CreaContatto

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+//catturiamo gli elementi che ci serviranno
 let contenitoreContatti = document.querySelector("#contenitoreContatti");
 let mostraContattiBtn = document.querySelector("#btn_Mostra"); // Modificato il selettore per adattarlo all'ID del pulsante
 let inputNome = document.querySelector("#inputNome");
 let inputNumero = document.querySelector("#inputNumero");
+
 const rubrica = {
     'contatti': [
         { nome: "Leonardo", numero: "34694030394" },
@@ -14,18 +16,24 @@ const rubrica = {
         { nome: "Sayo", numero: "3353625253256" },
     ],
     mostraContatti: function () {
+        //ciclo il mio array di contatti
         this.contatti.forEach((contatto) => {
+            //creo il div
             let cardContatto = document.createElement("div");
+            //aggiungo le classi
             cardContatto.classList.add("background-secondary", "p-5", "col-custom");
+            //aggiungo il contenuto del div andando a richiamare le proprietà nome e numero di ogni contatto nell'array contatti
             cardContatto.innerHTML = `
             <h6>${contatto.nome}</h6>
             <p>${contatto.numero}</p>
             `;
+            //appendo la card come figlio diretto del contenitore
             contenitoreContatti.appendChild(cardContatto);
         });
     },
 }
 rubrica.mostraContatti();
+//la variabile check serve per gestire i casi in cui non possiamo utilizzare il toggle (con d-none) e in cui vogliamo controllare se le nostre car erano state generato oppure no. Partiamo da false perchè all'inizio la funzione non è mai partita.
 let check = false;
 mostraContattiBtn.addEventListener("click", () => {
     if (!check) {
@@ -42,19 +50,22 @@ mostraContattiBtn.addEventListener("click", () => {
 document.querySelector("#btn_CreaContatto").addEventListener("click", () => {
     const nuovoNome = inputNome.value;
     const nuovoNumero = inputNumero.value;
-
+    //con il ciclo if metto tra () un confronto che mi rende vero il fatto che entrambi gli input siano inseriti prima di processare il ciclo.
     if (nuovoNome && nuovoNumero) {
-        const nuovoContatto = { nome: nuovoNome, numero: nuovoNumero };
-
+        const nuovoContatto = {nome: nuovoNome, numero: nuovoNumero};
         rubrica.contatti.push(nuovoContatto);
-
         contenitoreContatti.innerHTML = "";
         rubrica.mostraContatti();
-
+        check = true;
+        mostraContattiBtn.innerHTML = "Nascondi Contatti";
         inputNome.value = "";
         inputNumero.value = "";
     } else {
         alert("Inserisci un nome e un numero per il nuovo contatto.");
+        check = false;
+        mostraContattiBtn.innerHTML = "Mostra Contatti";
     }
 });
+
+
 
